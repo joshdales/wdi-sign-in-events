23f35792378a398e59a59eee389accbd9af17682
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-13 15:53:19 -0500

    Add fade in and fade out animations

--- js/main.js
+++ js/main.js
@@ -6,11 +6,18 @@ var signIn = document.querySelector('.signin');
 
 signIn.addEventListener('click', function() {
   modal.style.display = 'block';
+  setTimeout(function() {
+    modal.style.opacity = 0;
+    modal.style.opacity = 1;
+  }, 10);
 });
 
 var close = document.querySelector('.close');
 close.addEventListener('click', function() {
-  modal.style.display = 'none';
+  modal.style.opacity = 0;
+  setTimeout(function() {
+    modal.style.display = 'none';
+  }, 1000);
 })
 
 })
