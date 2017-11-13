a291a4d029e8cf8684d13fd1a0ac29afcc91d525
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-13 15:01:17 -0500

    Create sgn in form elements

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var container = document.createElement('div');
+var form = document.createElement('form');
+var usernameLabel = document.createElement('label');
+var usernameForm = document.createElement('input')
+var passwordLabel = document.createElement('label');
+var passwordForm = document.createElement('input');
